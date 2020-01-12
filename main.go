3c86bcb8949ef93b93cc580253fe988c1072e1e8
jhamPac <cloud@kevia.me>
2020-01-11 22:42:27 -0700

    pulled first argument using the os package

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Project start here")
+	firstArg := os.Args[1]
+	fmt.Printf("The first argument is %s", firstArg)
 }
