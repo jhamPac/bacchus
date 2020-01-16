4ca2b08339f73e64676507e7b8b378bb5a5708ee
jhamPac <cloud@kevia.me>
2020-01-16 09:53:18 -0700

    added file creation logic

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	firstArg := os.Args[1]
-	fmt.Printf("The first argument is %s", firstArg)
+	nameArg := os.Args[2]
+	typeArg := os.Args[4]
+	fmt.Printf("The file name is %s.js\nthe logic type is %s", nameArg, typeArg)
+	err := ioutil.WriteFile(nameArg+".js", []byte("import React from 'react'\nexport default function Marketing..."), 0755)
+
+	if err != nil {
+		fmt.Printf("Unable to write file: %v", err)
+	}
 }
